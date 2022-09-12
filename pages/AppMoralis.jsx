5b02b93ddf01a41a18a4ff481d80b25579ce6141
victoria rossi <victoria.rossi@agilesolutions.com>
2022-09-12 20:01:19 -0300

    Added title and text to AppMoralis

--- pages/AppMoralis.jsx
+++ pages/AppMoralis.jsx
@@ -6,6 +6,9 @@ import Moralis from "moralis-v1";
 function AppMoralis() {
     const { authenticate, isAuthenticated, isAuthenticating, user, account, logout } = useMoralis();
     const nftsData = []
+    let msg = "Click a button!";
+    let user_name = "";
+    let address = "";
 
     useEffect(() => {
         if (isAuthenticated) {
@@ -20,7 +23,8 @@ function AppMoralis() {
             .then(function (user) {
                 console.log("logged in user:", user);
                 console.log(user.get("ethAddress"));
-
+                document.getElementById("user").innerText += "User: "  + user.id + '\t'; 
+                document.getElementById("address").innerText += "Address: " + user.get("ethAddress"); 
             })
             .catch(function (error) {
                 console.log(error);
@@ -38,11 +42,15 @@ function AppMoralis() {
             console.log(nft.name + nft.metadata + '\n' + nft.token_address);
             nftsData.concat(nft.name + nft.metadata + '\n' + nft.token_address);
         }
+        document.getElementById("title").innerText="Logged in!"; 
     }
 
     const logOut = async () => {
         await logout();
         console.log("logged out");
+        document.getElementById("title").innerText="Logged out!"; 
+        document.getElementById("user").innerText = "User: Log in first"; 
+        document.getElementById("address").innerText = "Address: Log in first"; 
     }
 
   return (
@@ -50,6 +58,12 @@ function AppMoralis() {
           <h1>Moralis Hello World!</h1>
             <button onClick={login}>Moralis Metamask Login</button>
             <button onClick={logOut} disabled={isAuthenticating}>Logout</button>
+            <h1 id="title">{msg}</h1>
+            <div>
+                <h3>Data from wallet:</h3>
+                <p id="user">{user_name}</p>
+                <p id="address">{address}</p>
+            </div>
         </div>
     );
 }
