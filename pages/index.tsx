e6baa8ef21c5940ec3009abb84a1c81056c601cc
Marco444 <elscili444.code@gmail.com>
2022-09-25 12:00:38 -0300

    landing done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import type { NextPage } from "next";
-import AppMoralis from "../components/AppMoralis";
-import { MoralisProvider } from "react-moralis";
-import Landing from "./landing"
+import Head from "next/head";
+//import AppMoralis from "../components/AppMoralis";
+//import { MoralisProvider } from "react-moralis";
+import Landing from "./landing";
 
 const Home: NextPage = () => {
-    return (
-        // <MoralisProvider serverUrl="https://ktedylculcwo.usemoralis.com:2053/server" appId="CRpN1JvKz5OgW3XfTGhiXV1RTap72LEVoUjQN2Sk">
-        //     <AppMoralis />
-        // </MoralisProvider>
-        <Landing />
+  return (
+    // <MoralisProvider serverUrl="https://ktedylculcwo.usemoralis.com:2053/server" appId="CRpN1JvKz5OgW3XfTGhiXV1RTap72LEVoUjQN2Sk">
+    //     <AppMoralis />
+    // </MoralisProvider>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Landing />
+    </>
   );
 };
 
--- pages/landing.jsx
+++ pages/landing.jsx
@@ -1,41 +1,53 @@
-import { useEffect } from "react";
+import { Stack } from "@mui/system";
+import Image from "next/image";
+import image from "/public/products.png";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Fingerprint from "@mui/icons-material/Fingerprint";
 
 function Landing() {
-  useEffect(() => {
-    var frameNumber = 0, // start video at frame 0
-      // lower numbers = faster playback
-      playbackConst = 500,
-      // get page height from video duration
-      setHeight = document.getElementById("set-height"),
-      // select video element
-      vid = document.getElementById("v0");
-
-    // dynamically set the page height according to video length
-    vid.addEventListener("loadedmetadata", function () {
-      setHeight.style.height = Math.floor(vid.duration) * playbackConst + "px";
-    });
-
-    // Use requestAnimationFrame for smooth playback
-    function scrollPlay() {
-      var frameNumber = window.pageYOffset / playbackConst;
-      vid.currentTime = frameNumber;
-      window.requestAnimationFrame(scrollPlay);
-    }
-
-    window.requestAnimationFrame(scrollPlay);
-  }, []);
-
   return (
-    <div id="set-height">
-      <p id="time">
-        <video id="v0" tabIndex="0" autobuffer="autobuffer" preload="preload">
-          <source
-            type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
-            src="https://www.apple.com/media/us/mac-pro/2013/16C1b6b5-1d91-4fef-891e-ff2fc1c1bb58/videos/macpro_main_desktop.mp4"
-          ></source>
-        </video>
-      </p>
-    </div>
+    <>
+      <Stack style={{ paddingLeft: 333, paddingTop: 300 }} direction="row">
+        <Stack spacing="2">
+          <div
+            style={{
+              fontSize: 133,
+              fontWeight: "bold",
+              fontFamily: "Bebas Neue",
+            }}
+          >
+            CERTUS
+          </div>
+          <div
+            style={{
+              fontSize: 40,
+              fontWeight: "bold",
+              fontFamily: "Bebas Neue",
+            }}
+          >
+            BLOCKCHAIN BACKED AUTHENTICATION
+          </div>
+        </Stack>
+        <IconButton
+          aria-label="fingerprint"
+          color="secondary"
+          sx={{
+            ":hover": {
+              color: "#69f0ae",
+              bgcolor: "#2bbd7e",
+            },
+            color: "black",
+            height: 60,
+            width: 60,
+            marginTop: 10,
+            marginLeft: 20,
+          }}
+        >
+          <Fingerprint sx={{ fontSize: 60 }} />
+        </IconButton>
+      </Stack>
+    </>
   );
 }
 
