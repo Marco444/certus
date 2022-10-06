c2351eed1a875900d3b95791d9a9bc24bdeb4ed3
Marco444 <elscili444.code@gmail.com>
2022-10-06 09:41:36 -0300

    added basic grid and mapping

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import {UserProvider} from '../components/userContext';
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import { UserProvider } from "../components/userContext";
 import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MoralisProvider
-    serverUrl="https://fs1dakp51eab.usemoralis.com:2053/server"
-    appId="CMbB476u4cDHZMumKeGfvSItJbutvqhC2PNpgn5j"
-  >
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+      serverUrl="https://rbhdqxxttltw.grandmoralis.com:2053/server"
+      appId="XemYMntKGORFuirwocwynIuZK4QSBcZBU3Sfxynk"
+    >
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
     </MoralisProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/nfts.jsx
+++ pages/nfts.jsx
@@ -1,76 +1,58 @@
-import { Stack } from "@mui/system";
-import { Button } from "@mui/material";
-import Link from "next/link";
-
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../components/userContext";
-
-import { useMoralis } from "react-moralis";
+import GridLayout from "react-grid-layout";
+import { useNFTBalances } from "react-moralis";
 
 function Nfts() {
-
   const [isAuthenticated, authenticate, user, logout] = useContext(UserContext);
+  const { getNFTBalances, data, error, isLoading, isFetching } =
+    useNFTBalances();
+
+  const layout = [
+    { i: "a", x: 100, y: 0, w: 3, h: 2, minW: 4, maxW: 4 },
+    { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 4, maxW: 4 },
+  ];
+
+  const nfts = [
+    { key: "a", name: "test" },
+    { key: "b", name: "test" },
+  ];
+
+  const styles = {
+    backgroundColor: "darkblue",
+  };
+
+  const nftsPreviews = nfts.map((nft) => (
+    <div key={nft.key} style={styles}>
+      <a> {nft.name}</a>
+    </div>
+  ));
+
+  useEffect(() => {
+    getNFTBalances();
+  }, []);
 
   return (
     <>
       <div
         style={{
-          margin: 50,
-          fontSize: 133,
-          fontWeight: "normal",
+          margin: 10,
+          fontSize: 80,
+          fontWeight: "bold",
           fontFamily: "Bebas Neue",
         }}
       >
-        MY PRODUCTS:
+        MY PRODUCTS
       </div>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+      <GridLayout
+        className="layout"
+        layout={layout}
+        cols={12}
+        rowHeight={30}
+        width={1200}
       >
-        <Stack
-          style={{
-            margin: 50,
-          }}
-        >
-          <Link href="/nft" passHref>
-            <Button
-              sx={{
-                ":hover": {
-                  color: "#ffffff",
-                  bgcolor: "#4b4b4b",
-                },
-                color: "#ffffff",
-                bgcolor: "#1e1e1e",
-                maxHeight: 700,
-                minHeight: 70,
-                maxWidth: 700,
-                minWidth: 300,
-              }}
-            >
-              Adidas Sneaker
-            </Button>
-          </Link>
-          <Button
-              onClick={logout}
-            sx={{
-              ":hover": {
-                color: "#ffffff",
-                bgcolor: "#FF4754",
-              },
-              color: "#FF4754",
-              bcolor: "#FF4754",
-              maxHeight: 300,
-              maxWidth: 700,
-            }}
-          >
-            {" "}
-            LOG OUT{" "}
-          </Button>
-        </Stack>
-      </div>
+        {nftsPreviews}
+      </GridLayout>
     </>
   );
 }
