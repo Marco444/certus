d2087b6421c47d2bb2ce213522de6635cb27a4a7
victoria rossi <victoria.rossi@agilesolutions.com>
2022-10-20 09:07:32 -0300

    color styling

--- pages/index.jsx
+++ pages/index.jsx
@@ -40,14 +40,14 @@ const Home = () => {
       </Stack>
       <Stack className="landing" direction="row" spacing={10}>
         <Stack spacing={2}>
-          <div className="title-landing font">
+          <div className="title-landing font colorfont1">
             CERTUS
           </div>
-          <div className="description-landing font">
+          <div className="description-landing font colorfont1">
             BLOCKCHAIN BACKED AUTHENTICATION
           </div>
         </Stack>
-        <Login className="fingerprint-landing" authenticate={authenticateHandler} />
+        <Login className="fingerprint-landing colorfont1" authenticate={authenticateHandler} />
       </Stack>
     </>
   );
