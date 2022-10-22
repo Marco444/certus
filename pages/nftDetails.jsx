96ce8715dd0815926d4b765386618e7e57d18e5b
Marco444 <elscili444.code@gmail.com>
2022-10-22 17:43:03 -0300

    solved login with email

--- pages/nftDetails.jsx
+++ pages/nftDetails.jsx
@@ -4,30 +4,30 @@ import {useContext} from "react";
 import selectedNftContext from "../components/selectedNftContext";
 
 function NftDetails() {
-
-  const backHandler = () => {
-    router.push("/nfts");
-  };
-
-  const [selectedNft, setSelectedNft] = useContext(selectedNftContext);
-
-  const image = "";
-  console.log(metadata);
-  console.log(image);
-
-  return (
-    <div>
-        <div >
-            <ArrowBackIcon onClick={backHandler} className="backarrow colorfont"/>
-        </div>
-        <div>
-            <h1 className="myProducts font">{metadata.name}</h1>
-            <img src={"https://ipfs.io/ipfs/" + image} className="selectednft-image" ></img>
-            <p className="selectednft-image-desc">{metadata.description}</p>
-            <p className="selectednft-image-desc">token_hash: {selectedNft.token_hash}</p>
-        </div>
-    </div>
-  );
+  //
+  // const backHandler = () => {
+  //   router.push("/nfts");
+  // };
+  //
+  // const [selectedNft, setSelectedNft] = useContext(selectedNftContext);
+  //
+  // const image = "";
+  // console.log(metadata);
+  // console.log(image);
+  //
+  // return (
+  //   <div>
+  //       <div >
+  //           <ArrowBackIcon onClick={backHandler} className="backarrow colorfont"/>
+  //       </div>
+  //       <div>
+  //           <h1 className="myProducts font">{metadata.name}</h1>
+  //           <img src={"https://ipfs.io/ipfs/" + image} className="selectednft-image" ></img>
+  //           <p className="selectednft-image-desc">{metadata.description}</p>
+  //           <p className="selectednft-image-desc">token_hash: {selectedNft.token_hash}</p>
+  //       </div>
+  //   </div>
+  // );
 }
 
 export default NftDetails;
