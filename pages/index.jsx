f194e638e5f7b221e1f096ce9bdb5b1adc448ad5
Marco444 <elscili444.code@gmail.com>
2022-11-30 18:46:55 -0300

    testing style and deployment

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,49 +1,59 @@
-
 import { Stack } from "@mui/system";
 import { useRouter } from "next/router";
 import Login from "../components/Login";
 import { useContext } from "react";
 import UserContext from "../components/userContext";
 import { Button } from "@mui/material";
-import LoginForm from "../components/LoginForm"
-
+import LoginForm from "../components/LoginForm";
 
 const Home = () => {
-
-  const [isAuthenticated, authenticate, user, logout, userAddress, setUserAddress] = useContext(UserContext);
+  const [
+    isAuthenticated,
+    authenticate,
+    user,
+    logout,
+    userAddress,
+    setUserAddress,
+  ] = useContext(UserContext);
   const router = useRouter();
 
   const routeAbout = () => {
     router.push("/AboutUs");
-  }
+  };
 
-  const routeHow  = () => {
+  const routeHow = () => {
     router.push("/HowItWorks");
+  };
+
+  const buttonSx = {
+    fontSize: 18,
+    textDecoration: "underline",
+    color: "#010913",
+    '&:hover': { 
+      fontSize: 18,
+      backgroundColor: "#11e3ab"
+    }
   }
 
   return (
     <>
-      <Stack className="header" direction="row" spacing={4}>
-        <Button className="btn-landing font colorfont onPage">
-          Home
-        </Button>
-        <Button className="btn-landing font colorfont" onClick={routeAbout}>
+      <Stack direction="row" spacing={4}>
+        <Button sx={buttonSx} >Home</Button>
+        <Button onClick={routeAbout}>
           About us
         </Button>
-        <Button className="btn-landing font colorfont" onClick={routeHow}>
+        <Button onClick={routeHow}>
           How it works
         </Button>
       </Stack>
       <Stack className="landing" direction="row" spacing={10}>
         <Stack spacing={2}>
-          <div className="title-landing font colorfont1">
-            CERTUS
-          </div>
+          <div className="title-landing font colorfont1">CERTUSssssss</div>
           <div className="description-landing font colorfont1">
             BLOCKCHAIN BACKED AUTHENTICATION
           </div>
         </Stack>
-        <Login className="fingerprint-landing colorfont1"/>
+        <Login className="fingerprint-landing colorfont1" />
       </Stack>
     </>
   );
