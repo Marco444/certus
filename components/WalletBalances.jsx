e7f12e73ce714823aaec875225cfd425399cbf53
Marco444 <elscili444.code@gmail.com>
2022-12-03 19:42:25 -0300

    managed login without moralis

--- components/WalletBalances.jsx
+++ components/WalletBalances.jsx
@@ -8,20 +8,16 @@ function WalletBalance() {
     const [userAddress, setUserAddress,] = useContext(UserContext);
     const [nftBalance, setNftBalance] = useContext(NftBalanceContext);
 
-
-
-
     useEffect( () => {
 
         async function getBalance(){
             console.log("load balance")
-            // const contract = new ethers.Contract(tokenContractAddress, genericErc20Abi, provider);
-            // const balance = (await contract.balanceOf((await provider.getSigners())[0].address)).toString();
-            //const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
             const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const balance = await provider.getBalance(userAddress);
-            setNftBalance(ethers.utils.formatEther(balance));
-            console.log(nftBalance)
+           // const provider = ethers.providers.Web3Provider(window.ethereum/window.web3)
+            const contract = new ethers.Contract(tokenContractAddress, genericErc20Abi, provider);
+            const balance = (await contract.balanceOf((await provider.getSigners())[0].address)).toString();
+            setNftBalance(balance)
+            console.log(balance)
         }
 
         getBalance();
