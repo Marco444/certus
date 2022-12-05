a2169b1172828089a472a7861a8680c6700c1286
victoria rossi <vrossi@itba.edu.ar>
2022-12-05 01:08:30 -0300

    howitworks

--- pages/HowItWorks.jsx
+++ pages/HowItWorks.jsx
@@ -17,9 +17,9 @@ function HowItWorks() {
       <Header selected={"howItWorks"} />
       <div className="font aboutus-title">How it works</div>
       <p className="font howitworks">
-        First you have to login. You will have to place an e-mail. Then, if you don't have a 
+        First you have to login. You will have to place an e-mail. Then, if you don `&apos;`t have a 
         wallet linked with that e-mail, you will have to authorice the app through a mail.
-        Onces you've done that, you will be able to see all your NFTs. To do that please visit 
+        Onces you `&apos;`ve done that, you will be able to see all your NFTs. To do that please visit 
         the home page.
       </p>
     </>
